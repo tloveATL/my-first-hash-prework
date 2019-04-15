bb657a5a09c7640429eb55f57d05bea8ad44ad91
Tiffani Weaver <tlove.weaver@gmail.com>
2019-04-15 19:31:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -31,7 +31,7 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+shipping_manifest["muskets"]
 
   # add 4 gun powder to the shipping_manifest hash below
 
